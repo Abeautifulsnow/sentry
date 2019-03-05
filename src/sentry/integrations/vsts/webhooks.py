95a9852f0625e3da9a193c9c62753d06e3dec4c9
Katie Byers <lobsterkatie@gmail.com>
2019-03-04 21:58:22 -0800

    add reference link to vsts webhook

--- src/sentry/integrations/vsts/webhooks.py
+++ src/sentry/integrations/vsts/webhooks.py
@@ -44,7 +44,8 @@ class WorkItemWebhook(Endpoint):
         except KeyError as e:
             logger.info("vsts.invalid-webhook-payload", extra={"error": six.text_type(e)})
 
-        if event_type == "workitem.updated":
+        # https://docs.microsoft.com/en-us/azure/devops/service-hooks/events?view=azure-devops#workitem.updated
+        if event_type == 'workitem.updated':
             try:
                 integration = Integration.objects.get(
                     provider=PROVIDER_KEY, external_id=external_id
